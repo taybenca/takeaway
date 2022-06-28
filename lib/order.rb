82508113fbfe26ad81ea1f8a89bd1cec9b361286
Tayanne Bencardino <tayannebencardino@gmail.com>
2022-06-28 12:18:08 +0100

    Changind tests

--- lib/order.rb
+++ lib/order.rb
@@ -20,9 +20,9 @@ require 'twilio-ruby'
 class Order
   def initialize(order)
     @order = order
-    @chosen_itens = []
+    @chosen_items = []
     @menu = {"Steak" => 14, "Feijoada" => 10, "Moqueca" => 12, "Passion Fruit Cream" => 4, "Cake" => 5, "Brigadeiro" => 2, "Mango Juice" => 5, "Coffee" => 3, "Caipirinha" => 6}
-    @chosen_itens_with_price = Hash.new
+    @chosen_items_with_price = Hash.new
   end
 
   def format(hash)
@@ -37,15 +37,15 @@ class Order
     puts "Please write the item you'd like to order:"
     format(@menu)
     puts "For checkout, press 0"
-    # The user can add other itens and when they finishes, they press 0.
-    # chosen_itens is a array with keys
+    # The user can add other items and when they finishes, they press 0.
+    # chosen_items is a array with keys
     while user_input != "0" do
       user_input = @order.gets.chomp
-      @chosen_itens << user_input if @menu.include?(user_input)
+      @chosen_items << user_input if @menu.include?(user_input)
     end
       # Now we need to find the value of each key listed an add in a new hash
-    @chosen_itens.each do |item| 
-    @chosen_itens_with_price.merge!(
+    @chosen_items.each do |item| 
+    @chosen_items_with_price.merge!(
       @menu.select do |key, value|
         key.include?(item)
       end)
@@ -55,7 +55,7 @@ class Order
 
   def checkout
     puts "Your order:"
-    format(@chosen_itens_with_price)
+    format(@chosen_items_with_price)
     total
     puts "Press 1 to confirm, 2 to delete or 3 to add"
     user_input = @order.gets.chomp
@@ -71,7 +71,7 @@ class Order
 
   def total
     total_price = []
-    @chosen_itens_with_price.each do |key, value|
+    @chosen_items_with_price.each do |key, value|
       total_price << value
     end
     puts "\nTotal: £#{total_price.sum}"
@@ -100,13 +100,13 @@ class Order
 
   def delete
     puts "Please write the item to delete:"
-    format(@chosen_itens_with_price)
+    format(@chosen_items_with_price)
     user_input = @order.gets.chomp
-    @chosen_itens.delete(user_input)
-    @chosen_itens_with_price.delete(user_input)
+    @chosen_items.delete(user_input)
+    @chosen_items_with_price.delete(user_input)
     checkout
   end
 end
 
-order = Order.new(Kernel)
-order.choose_order
+# order = Order.new(Kernel)
+# order.choose_order
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -3,16 +3,44 @@ require 'order'
 describe Order do
   it 'returns the format of the menu' do
     order = Order.new(Kernel)
-    expect(order.format(Kernel)).to eq "Steak" => 14, "Feijoada" => 10, "Moqueca" => 12, "Cake" => 5, "Juice" => 5, "Caipirinha" => 6
+    menu = {"Steak" => 14}
+    result = order.format(menu)
+    expect(result).to eq menu
   end
 
   it 'choose one item and return the list' do
-    fake_order = double(:fake_menu)
+    fake_order = double(:fake_menu, gets: "Feijoada")
     expect(fake_order).to receive(:gets).and_return("Feijoada")
-    expect(fake_order).to receive(:gets).and_return("Juice")
+    expect(fake_order).to receive(:gets).and_return("Mango Juice")
     expect(fake_order).to receive(:gets).and_return("0")
     order = Order.new(fake_order)
     order.choose_order
-    order.checkout
+  end
+
+  xit 'send sms after order is completed' do
+    fake_sms = double(:fake_sms)
+    time_format = (Time.now+1800).strftime("%k:%M")
+    expect(fake_sms).to receive(:criate)
+      .with(body: "Thank you! Your order was placed and will be delivered before #{time_format}",
+      from: '+18599558642',
+      to: '+447472956139')
+    order = Order.new(fake_sms)
+    order.send_sms
+  end
+
+  it 'delete an item' do
+    fake_item = double(:fake_item, gets: "Cake")
+    expect(fake_item).to receive(:gets).and_return("Cake")
+    order = Order.new(fake_item)
+    order.delete
+  end
+
+  it 'check the delivery time' do
+    fake_time = double(:fake_time)
+    fake_time = Time.new(2022, 6, 28, 12, 14, 33)+1800
+    result = fake_time.strftime("%k:%M")
+    expect(result).to eq "12:44"
+    order = Order.new(fake_time)
+    order.time_format
   end
 end
